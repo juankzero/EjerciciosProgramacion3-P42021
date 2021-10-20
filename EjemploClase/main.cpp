94a66d4ce87cf6973226ba3dc80d8d4be7d066ac
Juan Zepeda <juan.zero@unitec.edu>
2021-10-19 20:00:17 -0600

    Se comienza el ejercicio con Clase en C++

---
+++ EjemploClase/main.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <conio.h>
+
+class Numero
+{
+private:
+	int valor;
+
+public:
+	Numero() 
+	{}
+
+	Numero(int _valor) 
+	{}
+
+	void setValor(int _valor) 
+	{}
+
+	int getValor() 
+	{}
+
+	bool esPar()
+	{}
+
+	bool esPrimo() 
+	{}
+
+	bool esMultiplo(Numero) 
+	{}
+
+};
+
+int main() 
+{
+	Numero n1;
+	Numero n2(11);
+
+
+	_getch();
+}
